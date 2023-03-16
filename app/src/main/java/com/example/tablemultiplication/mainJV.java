ee13cf55ea803dcd49a912533b6270fd631efa41
Ahbaly <123125056+Ahbaly@users.noreply.github.com>
2023-03-16 23:09:37 +0100

    commit initial

---
+++ app/src/main/java/com/example/tablemultiplication/mainJV.java
@@ -0,0 +1,16 @@
+package com.example.tablemultiplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainJV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_ac);
+
+        setTitle("TPC04.EX02");
+    }
+}
