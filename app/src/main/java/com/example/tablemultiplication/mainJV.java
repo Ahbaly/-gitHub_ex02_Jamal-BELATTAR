96112df244e4253cda1ddaf35c224e2d8b7de66a
Ahbaly <123125056+Ahbaly@users.noreply.github.com>
2023-03-17 00:13:27 +0100

    modify mainJV: program buttons reinitialiser&quitter

--- app/src/main/java/com/example/tablemultiplication/mainJV.java
+++ app/src/main/java/com/example/tablemultiplication/mainJV.java
@@ -3,14 +3,54 @@ package com.example.tablemultiplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class mainJV extends AppCompatActivity {
+public class mainJV extends AppCompatActivity
+{
+    EditText nbr;
+    Button reinitialiser, quitter;
+    String txt;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_ac);
 
         setTitle("TPC04.EX02");
+
+        nbr = findViewById(R.id.etNbr);
+        reinitialiser = findViewById(R.id.btnRntlsr);
+        quitter = findViewById(R.id.btnQtr);
+
+        reinitialiser.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                txt = String.format("%s", nbr.getText());
+
+                if (txt.isEmpty())
+                {
+                    Toast.makeText(mainJV.this, "aslan khawi", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    nbr.setText("");
+                }
+            }
+        });
+
+        quitter.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                System.exit(0);
+            }
+        });
     }
 }
