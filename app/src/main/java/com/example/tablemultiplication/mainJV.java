9e33deb6bd4c7c03ad665b1b39152b4a824c9caf
Ahbaly <123125056+Ahbaly@users.noreply.github.com>
2023-03-17 23:21:42 +0100

    modify mainJV: bug fixes

--- app/src/main/java/com/example/tablemultiplication/mainJV.java
+++ app/src/main/java/com/example/tablemultiplication/mainJV.java
@@ -2,29 +2,34 @@ package com.example.tablemultiplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class mainJV extends AppCompatActivity
 {
     EditText nbr;
     Button reinitialiser, quitter;
-    String txt;
+    String txt, error;
+    TextView multiplication;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_ac);
 
-        setTitle("TPC04.EX02");
+        setTitle("TABLE DE MULTIPLICATION");
 
         nbr = findViewById(R.id.etNbr);
         reinitialiser = findViewById(R.id.btnRntlsr);
         quitter = findViewById(R.id.btnQtr);
+        multiplication = findViewById(R.id.tvMltplct);
 
         reinitialiser.setOnClickListener(new View.OnClickListener()
         {
@@ -35,11 +40,28 @@ public class mainJV extends AppCompatActivity
 
                 if (txt.isEmpty())
                 {
-                    Toast.makeText(mainJV.this, "aslan khawi", Toast.LENGTH_SHORT).show();
+                    error = "aslan edit text khawi";
+
+                    txt = String.format("%s", multiplication.getText());
+                    if (txt.charAt(0) == '?')
+                    {
+                        error += " and TABLE DE MULTIPLICATION madarch";
+                    }
+                    else
+                    {
+                        multiplication.setText(R.string.multpleTv);
+                    }
+
+                    Toast.makeText(mainJV.this, error, Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
                     nbr.setText("");
+
+                    if (txt.charAt(0) != '?')
+                    {
+                        multiplication.setText(R.string.multpleTv);
+                    }
                 }
             }
         });
